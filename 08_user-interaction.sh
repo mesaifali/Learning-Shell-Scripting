d252cc34c69f6239fb70cecb8faec93f08524fda
mesaifali <mdsaifali1122@gmail.com>
2024-02-23 18:59:24 +0545

    input from user using -p

--- 08_user-interaction.sh
+++ 08_user-interaction.sh
@@ -4,6 +4,6 @@ echo "Enter Your Name:"
 read name
 echo "My name is $name"
 
-#taking input another -p method
-read -p "what is your name" name  #name is variable
+#taking input from user using -p method
+read -p "what is your name : " name  #name is variable
 echo "My name is $name"
